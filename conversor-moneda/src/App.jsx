9887db7eae7ce8238d99fea3f6a6b2dcc9a9e2ac
juanmabo <juanmanuelsf2211@gmail.com>
2024-08-16 01:32:21 -0300

    ejercicio react conversor

---
+++ conversor-moneda/src/App.jsx
@@ -0,0 +1,32 @@
+import { useRef } from 'react'
+import './App.css'
+
+function App() {
+  const dolarRef = useRef();
+ 
+  const resultref = useRef();
+
+  const calcular = ()=> {
+    const dolarValor = dolarRef.current.value;
+
+    const pesosArg = dolarValor*988;
+
+    resultref.current.innerHTML=pesosArg;
+
+  }
+  return (
+    <div>
+
+      <h1>Conversor Dolar-Peso Argentino</h1>
+      <input type="text" ref={dolarRef} />
+      <button onClick={calcular}>Convertir</button>
+      <div ref={resultref}>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
